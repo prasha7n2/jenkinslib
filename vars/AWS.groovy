c0cfb6f8fe85ce1b27ca6004407e9a26115da4ad
prasha7n2 <43166284+prasha7n2@users.noreply.github.com>
2020-04-04 18:38:56 +0530

    Update AWS.groovy

--- vars/AWS.groovy
+++ vars/AWS.groovy
@@ -1,6 +1,6 @@
 def call(command) {
     try {
-        docker.image("releaseworks/awscli:latest").inside("--entrypoint \"\" -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION") {
+        docker.image("releaseworks/awscli").inside("--entrypoint \"\" -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION") {
             sh "aws ${command}"
         }
     } catch( ClassNotFoundException e ) {
